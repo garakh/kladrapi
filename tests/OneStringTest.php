37d9dba28179128c75f034d02bb49b03d885454f
Yurii Lichutin <yuriilichutin@hotmail.com>
2014-06-19 10:49:15 +0400

    new tests

--- tests/OneStringTest.php
+++ tests/OneStringTest.php
@@ -6,8 +6,7 @@
  * @author Y. Lichutin
  */
 class OneStringTest extends PHPUnit_Framework_TestCase
-{
-    
+{  
     /*
      * Тест поиска области
      */
@@ -25,24 +24,7 @@ class OneStringTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($res[0]->name, 'Архангельская');
         $this->assertEQuals($res[0]->regionId, 2900000000000);
     }
-    /*
-     * Тест поиска области с ключевыми словами
-     */
-//    public function testRegionSearch2()
-//    {
-//        $query = new QueryToApi();
-//        $query->oneString = true;
-//        $query->query = 'еврейск ао';
-//        $query->limit = 1;
-//        
-//        $res = $query->send();
-//        $res = $res->result;
-//        
-//        $this->assertEquals($res[0]->contentType, QueryToApi::RegionType);
-//        $this->assertEquals($res[0]->name, 'Еврейская');
-//        $this->assertEQuals($res[0]->regionId, 7900000000000);
-//    }  
-    
+
     /*
      * Тест поиска района
      */
@@ -50,7 +32,7 @@ class OneStringTest extends PHPUnit_Framework_TestCase
     {
         $query = new QueryToApi();
         $query->oneString = true;
-        $query->query = 'краснодарский брюх';
+        $query->query = 'краснодарский кр брюховецкий р-н';
         $query->limit = 1;
         
         $res = $query->send();
@@ -61,25 +43,6 @@ class OneStringTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($res[0]->regionId, 2300000000000);
         $this->assertEquals($res[0]->districtId, 2300700000000);
     }
-    
-    /*
-     * Тест поиска района с использованием ключевых слов
-     */
-//    public function testDistrictSearch2()
-//    {
-//        $query = new QueryToApi();
-//        $query->oneString = true;
-//        $query->query = 'респ карелия кем район';
-//        $query->limit = 1;
-//        
-//        $res = $query->send();
-//        $res = $res->result;
-//        
-//        $this->assertEquals($res[0]->contentType, QueryToApi::DistrictType);
-//        $this->assertEquals($res[0]->name, 'Кемский');
-//        $this->assertEquals($res[0]->regionId, 1000000000000);
-//        $this->assertEquals($res[0]->districtId, 1000400000000); 
-//    }
     
     /*
      * Тест поиска города
@@ -100,26 +63,7 @@ class OneStringTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($res[0]->districtId, 5000400000000); 
         $this->assertEquals($res[0]->cityId, 5000400000800);
     }
-    
-    /*
-     * Тест поиска города с использованием ключевых слов
-     */
-//    public function testCitySearch2()
-//    {
-//        $query = new QueryToApi();
-//        $query->oneString = true;
-//        $query->query = 'г санкт-петербург';
-//        $query->limit = 1;
-//        
-//        $res = $query->send();
-//        $res = $res->result;
-//        
-//        $this->assertEquals($res[0]->contentType, QueryToApi::CityType);
-//        $this->assertEquals($res[0]->name, 'Санкт-Петербург');
-//        $this->assertEquals($res[0]->regionId, 7800000000000);
-//        $this->assertEquals($res[0]->cityId, 7800000000000);
-//    }
-//    
+  
     /*
      * Тест поиска улицы
      */
@@ -127,7 +71,7 @@ class OneStringTest extends PHPUnit_Framework_TestCase
     {
         $query = new QueryToApi();
         $query->oneString = true;
-        $query->query = 'мезенский каменка лесна';
+        $query->query = 'мезенский р каменка ул лесна';
         $query->limit = 1;
         
         $res = $query->send();
@@ -142,33 +86,13 @@ class OneStringTest extends PHPUnit_Framework_TestCase
     }
     
     /*
-     * Тест поиска улицы с использованием ключевых слов
-     */
-//    public function testStreetSearch2()
-//    {
-//        $query = new QueryToApi();
-//        $query->oneString = true;
-//        $query->query = 'архангельская обл г архангельск переулок Физкультурный';
-//        $query->limit = 1;
-//        
-//        $res = $query->send();
-//        $res = $res->result;
-//        
-//        $this->assertEquals($res[0]->contentType, QueryToApi::StreetType);
-//        $this->assertEquals($res[0]->name, '1-й Физкультурный');
-//        $this->assertEquals($res[0]->regionId, 2900000000000);
-//        $this->assertEquals($res[0]->cityId, 2900000100000);
-//        $this->assertEquals($res[0]->streetId, 29000001000000200);
-//    }
-    
-    /*
      * Тест поиска здания
      */
     public function testBuildingSearch1()
     {
         $query = new QueryToApi();
         $query->oneString = true;
-        $query->query = 'петрозаводск ленина 35а';
+        $query->query = 'петрозаводск ленина д 35а';
         $query->limit = 1;
         
         $res = $query->send();
@@ -181,27 +105,7 @@ class OneStringTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($res[0]->streetId, 10000001000013900); 
         $this->assertEquals($res[0]->buildingId, 1000000100001390004);
     }
-    
-    /*
-     * Тест поиска здания с использованием ключевых слов
-     */
-//    public function testBuildingSearch2()
-//    {
-//        $query = new QueryToApi();
-//        $query->oneString = true;
-//        $query->query = 'г орел ул колхозная дом 6';
-//        $query->limit = 1;
-//        
-//        $res = $query->send();
-//        $res = $res->result;
-//        
-//        $this->assertEquals($res[0]->contentType, QueryToApi::BuildingType);
-//        $this->assertEquals($res[0]->name, 'д. 6');
-//        $this->assertEquals($res[0]->regionId, 3500000000000);
-//        $this->assertEquals($res[0]->cityId, 3502200009800);
-//        $this->assertEquals($res[0]->streetId, 35022000098000300); 
-//        $this->assertEquals($res[0]->buildingId, 3502200009800030001);        
-//    }
+
     
     /*
      * Проверяет правильность работы лимита
@@ -235,5 +139,59 @@ class OneStringTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($query->limit, count($res));
     }
     
-    //написать тесты на корректность лимита разных типов
+    /*
+     * Проверяет, что дома занимают половину лимита при нормальном запросе
+     */
+    public function testBuildingLimitNormal()
+    {
+        $query = new QueryToApi();
+        $query->oneString = true;
+        $query->query = 'москва архангельский 1';
+        $query->limit = 5;
+        
+        $res = $query->send();
+        $res = $res->result;
+        
+        $buildCount = 0;
+        
+        foreach ($res as $answer)
+        {
+            if ($answer->contentType == 'building')
+            {
+                $buildCount += 1;
+            }
+        }
+        
+        $this->assertEquals(ceil($query->limit/2), $buildCount);
+    }
+    
+    /*
+     * Проверяет, что дома занимают всё свободное место в лимите, если остальных объектов меньше, чем половина лимита
+     */
+    public function testBuildingLimitLess()
+    {
+        $query = new QueryToApi();
+        $query->oneString = true;
+        $query->query = 'ярославль ленина 1';
+        $query->limit = 20;
+        
+        $res = $query->send();
+        $res = $res->result;
+        
+        $buildCount = 0;
+        $otherCount = 0;
+        foreach ($res as $answer)
+        {
+            if ($answer->contentType == 'building')
+            {
+                $buildCount += 1;
+            }
+            else
+            {
+                $otherCount += 1;
+            }
+        }
+        
+        $this->assertEquals($query->limit, $buildCount+$otherCount);
+    }
 }
