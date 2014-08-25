304acb2a193e08a0c95b90de8ee1f8bca84ad761
Iliya Garakh <garakh@primepix.ru>
2014-08-26 00:04:52 +0400

    добавил гугло-форум

---
+++ apps/frontend/controllers/ForumController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Kladr\Frontend\Controllers {
+
+    use \Phalcon\Tag,
+        \Phalcon\Mvc\Controller;
+
+    class ForumController extends Controller
+    {
+
+        public function initialize()
+        {
+            Tag::setTitle('Форум с Кладр в облаке');
+            $this->view->setVar('description', 'Описание интеграции с Кладр в облаке. Модули для jQuery, PHP, .Net, 1C-Bitrix');
+            $this->view->setVar('keywords', 'КЛАДР, ФИАС, скачать КЛАДР, скачать ФИАС, скачать базу КЛАДР, скачать базу ФИАС, доступ к базе КЛАДР, доступ к базе ФИАС, КЛАДР онлайн, ФИАС онлайн, структура базы КЛАДР, структура базы ФИАС, описание базы КЛАДР, описание базы ФИАС, jQuery, php, net, 1c-bitrix');
+        }
+
+        public function indexAction()
+        {
+            
+        }
+
+    }
+
+}
