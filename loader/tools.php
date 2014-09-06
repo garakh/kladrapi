995c168e0e4aff489d74a01f7da62e3b2823f3b3
Iliya Garakh <garakh@primepix.ru>
2014-09-07 02:33:31 +0400

    у которых в имени встречается знак «/».

--- loader/tools.php
+++ loader/tools.php
@@ -3,7 +3,7 @@
 function __normalize($str){
     $str = preg_replace('/\./u', ' ', $str);
 	$str = preg_replace('/[Ёё]/u', 'е', $str);
-    $str = preg_replace('/[^а-яА-Я0-9,\s]+/u', '', $str);
+    $str = preg_replace('/[^а-яА-Я0-9,\s\/]+/u', '', $str);
     $str = mb_strtolower($str, 'UTF-8');
     
     $arRes = array();
