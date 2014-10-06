f4e66b1a6b8469820a19635b546c2df4aac912c3
Iliya Garakh <garakh@primepix.ru>
2014-10-07 01:39:18 +0400

    Фикс стилей Фикс нормализации слов Документация

--- apps/core/plugins/tools/Tools.php
+++ apps/core/plugins/tools/Tools.php
@@ -12,81 +12,85 @@ namespace Kladr\Core\Plugins\Tools {
     class Tools
     {
 
-        /**
-         * Нормализует строку
-         * 
-         * @param string $str
-         * @return string
-         */
-        public static function Normalize($str)
-        {
-            $str = preg_replace('/[^а-яА-Я0-9,\/]+/u', '', $str); 
-            $str = mb_strtolower($str, mb_detect_encoding($str));
+	/**
+	 * Нормализует строку
+	 * 
+	 * @param string $str
+	 * @return string
+	 */
+	public static function Normalize($str)
+	{
+	    $str = preg_replace('/[^а-яА-Я0-9,\/._]+/u', '', $str);
+	    $str = mb_strtolower($str, mb_detect_encoding($str));
 
-            return $str;
-        }
+	    return $str;
+	}
 
-        /**
-         * Обертка над mb_strlen c анализом кодировки
-         * 
-         * @param string $str Строка
-         * @return int Длина строки
-         */
-        public static function Strlen($str)
-        {
-            return mb_strlen($str, mb_detect_encoding($str));
-        }
-        
-        /**
-         * Конвертирует строку на английском в строку на русском в 
-         * соответвии с windows раскладкой клавиатуры
-         * 
-         * @param string $strMessage
-         * @return string
-         */
-        public static function Key($strMessage)
-        {
-            $s1 = "qazwsxedcrfvtgbyhnujmik,ol.p;[']-1234567890 ";
-            $s2 = "йфяцычувскамепинртгоьшлбщдюзжхэъ-1234567890 ";
+	/**
+	 * Обертка над mb_strlen c анализом кодировки
+	 * 
+	 * @param string $str Строка
+	 * @return int Длина строки
+	 */
+	public static function Strlen($str)
+	{
+	    return mb_strlen($str, mb_detect_encoding($str));
+	}
 
-            $s12 = "QAZWSXEDCRFVTGBYHNUJMIK<OL>P:{\"} ";
-            $s22 = "ЙФЯЦЫЧУВСКАМЕПИНРТГОЬШЛБЩДЮЗЖХЭЪ ";
+	/**
+	 * Конвертирует строку на английском в строку на русском в 
+	 * соответвии с windows раскладкой клавиатуры
+	 * 
+	 * @param string $strMessage
+	 * @return string
+	 */
+	public static function Key($strMessage)
+	{
+	    //Если латинских символов нет, то раскладка верная — вернем строку, как она к нам пришла
+	    if (!preg_match('/[a-zA-Z]/u', $strMessage))
+		return $strMessage;
+
+	    $s1 = "qazwsxedcrfvtgbyhnujmik,ol.p;[']-1234567890 ";
+	    $s2 = "йфяцычувскамепинртгоьшлбщдюзжхэъ-1234567890 ";
+
+	    $s12 = "QAZWSXEDCRFVTGBYHNUJMIK<OL>P:{\"} ";
+	    $s22 = "ЙФЯЦЫЧУВСКАМЕПИНРТГОЬШЛБЩДЮЗЖХЭЪ ";
 
 
-            $strNew = '';
-            for ($i = 0; $i < mb_strlen($strMessage, mb_detect_encoding($strMessage)); $i++)
-            {
-                $char = mb_substr($strMessage, $i, 1, mb_detect_encoding($strMessage));
+	    $strNew = '';
+	    for ($i = 0; $i < mb_strlen($strMessage, mb_detect_encoding($strMessage)); $i++)
+	    {
+		$char = mb_substr($strMessage, $i, 1, mb_detect_encoding($strMessage));
 
-                if (strpos($s2, $char) !== false)
-                {
-                    $strNew .= $char;
-                    continue;
-                }
+		if (strpos($s2, $char) !== false)
+		{
+		    $strNew .= $char;
+		    continue;
+		}
 
-                if (strpos($s22, $char) !== false)
-                {
-                    $strNew .= $char;
-                    continue;
-                }
+		if (strpos($s22, $char) !== false)
+		{
+		    $strNew .= $char;
+		    continue;
+		}
 
-                if (strpos($s1, $char) !== false)
-                {
-                    $p = strpos($s1, $char);
-                    $strNew .= mb_substr($s2, $p, 1, mb_detect_encoding($s2));
-                    continue;
-                }
+		if (strpos($s1, $char) !== false)
+		{
+		    $p = strpos($s1, $char);
+		    $strNew .= mb_substr($s2, $p, 1, mb_detect_encoding($s2));
+		    continue;
+		}
 
-                if (strpos($s12, $char) !== false)
-                {
-                    $p = strpos($s12, $char);
-                    $strNew .= mb_substr($s22, $p, 1, mb_detect_encoding($s22));
-                    continue;
-                }
-            }
+		if (strpos($s12, $char) !== false)
+		{
+		    $p = strpos($s12, $char);
+		    $strNew .= mb_substr($s22, $p, 1, mb_detect_encoding($s22));
+		    continue;
+		}
+	    }
 
-            return $strNew;
-        }
+	    return $strNew;
+	}
 
     }
 
